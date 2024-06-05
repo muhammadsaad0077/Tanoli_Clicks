8579ec1749e04965a046866917c80a555326b265
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-06-05 21:49:38 +0500

    Animations and Footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/heading-has-content */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../Pictures/tc_logo.jpg'
 import '../App.css'
 import pic1 from "../Pictures/pic1.jpg"
@@ -17,54 +16,86 @@ import pic12 from "../Pictures/pic12.jpg"
 import pic13 from "../Pictures/pic13.webp"
 import pic14 from "../Pictures/pic14.webp"
 import pic15 from "../Pictures/pic15.webp"
+import insta from "../Pictures/Instagram_icon.png.webp"
+
 const Home = () => {
-  let images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15]
+  const [lastScrollTop, setLastScrollTop] = useState(0)
+  const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15]
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop
+      const scrollDirection = currentScrollTop > lastScrollTop ? 'down' : 'up'
+      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop)
+
+      const elements = document.querySelectorAll('.slide-in')
+      elements.forEach((el) => {
+        const rect = el.getBoundingClientRect()
+        if (rect.top < window.innerHeight && rect.bottom > 0 && !el.classList.contains('animated')) {
+          el.classList.add('animated')
+          el.classList.add(scrollDirection === 'down' ? 'animate-slideInLeft' : 'animate-slideInRight')
+        }
+      })
+    }
+
+    // Initial animation for images on page load
+    const initialElements = document.querySelectorAll('.slide-in')
+    initialElements.forEach((el) => {
+      const rect = el.getBoundingClientRect()
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        el.classList.add('animated', 'animate-slideInLeft')
+      }
+    })
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [lastScrollTop])
+
   return (
-    <div>  
-      <div className='tagline flex mx-[330px] gap-[120px] whitespace-nowrap'>
-      <h1 className='my-[30px]'>BRING YOUR CREATIVITY</h1>
-      <h1 className='my-[30px] tagline2'>TO LIFE</h1>
+    <div>
+      <div className='tagline flex tracking-[-1px] md:tracking[10px] mx-[5px] md:mx-[330px] gap-[120px] whitespace-nowrap'>
+        <h1 className='my-[30px] hidden md:block animate-fadeIn'>BRING YOUR CREATIVITY</h1>
+        <h1 className='my-[30px] mx-1 block md:hidden tracking-wider animate-fadeIn'>BRING YOUR</h1>
+        <h1 className='my-[30px] hidden md:block tracking-[40px] animate-fadeIn'>TO LIFE</h1>
+        <h1 className='my-[30px] tracking-normal block md:hidden animate-fadeIn'>CREATIVITY TO LIFE</h1>
       </div>
       <div>
-        
         <img alt='logo' className='w-[70px] h-[70px] rounded-lg mx-auto my-[-56px] border border-b-[3px] border-black' src={logo}></img>
         <h1 className='solid my-[23px]'></h1>
       </div>
       <div className='fontstyle text-center my-16'>
-        <h1 className='text-[35px]'>Tanoli</h1>
-        <h1>Clicks</h1>
+        <h1 className='text-[35px] animate-fadeIn'>Tanoli</h1>
+        <h1 className='animate-fadeIn'>Clicks</h1>
       </div>
 
-    <div className='flex flex-wrap'>
-      <div className='flex flex-wrap gap-10 mx-44'>
-        {
-          images.map((img, index)=>{
-            return <img className='h-[350px] w-[350px] rounded-lg' alt='pic' key={index} src={img}></img>
-          })
-          
-        }
+      <div className='flex flex-wrap'>
+        <div className='flex flex-wrap gap-10 mx-10 md:mx-44'>
+          {
+            images.map((img, index) => {
+              return <img className='md:h-[350px] md:w-[350px] w-[300px] h-[300px] rounded-lg slide-in' alt='pic' key={index} src={img}></img>
+            })
+          }
+        </div>
       </div>
-    </div>
-    <div class="footer mt-24">
-      <div class="row">
-        <a href="#"><i class="facebook"></i></a>
-        <a href="#"><i class="instagram"></i></a>
-        <a href="#"><i class="youtube"></i></a>
-        <a href="#"><i class="twitter"></i></a>
+      <div className="footer mt-24">
+        <div className='w-8 h-8 mx-auto'>
+          <a className='w-4 h-4' href="https://instagram.com/tanoliclicks"><img src={insta} alt='logo'></img></a>
+        </div>
+        <div className="row">
+          <ul>
+            <li><a href="#">Book Now</a></li>
+            <li><a href="#">Our Services</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+          </ul>
+        </div>
+        <div className="row relative md:top-0 top-5">
+          TanoliClicks Copyright @ 2023 TanoliClicks - All rights reserved | Developed by Saad
+        </div>
       </div>
-      <div class="row">
-        <ul>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Our Services</a></li>
-          <li><a href="#">My Projects</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Terms & Conditions</a></li>         
-        </ul>
-      </div>
-      <div class="row">
-        TanoliClicks Copyright @ 2023 TanoliClicks - All rights reserved | Designed by Saad
-      </div> 
-    </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideInRight: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.5s ease-out forwards',
+        slideInLeft: 'slideInLeft 1s ease-out forwards',
+        slideInRight: 'slideInRight 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
-
